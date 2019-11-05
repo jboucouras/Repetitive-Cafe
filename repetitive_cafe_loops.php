4cf436fef5bf8387f2f156e33b495ce6113faa05
Jason Boucouras <55840268+jboucouras@users.noreply.github.com>
2019-11-04 20:29:34 -0500

    Add files via upload

---
+++ repetitive_cafe_loops.php
@@ -0,0 +1,29 @@
+<h1>Welcome to the Repetitive Cafe</h1>
+<?php
+$drinks = [
+  "Latte" => 3.99,
+  "Coffee" => 2.00,
+  "Tea" => 2.50,
+  "Mocha" => 4.50
+];
+?>
+<?php
+$pastries = [
+  "Muffin",
+  "Croissant",
+  "Slice of Pie",
+  "Cupcake"
+];
+?>
+<h3>Drinks!</h3>
+<ul>
+<?php foreach($drinks as $drink=>$price):?>
+<li><?= "$drink: $$price"?></li>
+<?php endforeach;?>
+</ul>
+<h3>Pastries! ($2 each)</h3>
+<ul>
+<?php for($i = 0; $i<count($pastries); $i++):?>
+<li><?=$pastries[$i]?></li>
+<?php endfor;?>
+</ul>
